a7019e41e4615432edd5ae1c04a348cec9025635
fsfaysalcse <faysal@heyjom.com>
2024-02-13 03:31:24 +0800

    udpafd

--- app.py
+++ app.py
@@ -16,3 +16,8 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True, host='0.0.0.0', port=9090)
+
+
+
+
+#/Users/fsfaysalcse/Desktop/FaceRec/venv/bin/python -m gunicorn -w 4 -b 0.0.0.0:9090 app:app
